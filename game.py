7892ed529ad7619c42e2e7bcc7b47c00574621eb
samtpjerry-lgtm <samtpjerry@gmail.com>
2026-09-11 09:24:18 +1200

    Restarted. Game Dimentions, Start and colour coded

--- game.py
+++ game.py
@@ -1,31 +1,25 @@
 import tkinter as tk
 import random
 
-print('test')
-print('test')
- 
-# ============================================================
-# QUIZ + PIXEL MAZE GAME
-# ============================================================
- 
-class QuizMazeGame:
+
+# Gives the game a title and sets colour and dimentions of the game
+
+
+class QuizGame:
     def __init__(self, root):
         self.root = root
-        self.root.title("Quiz Maze")
+
+        # Window settings
+        self.root.title("Quiz Game")
         self.root.geometry("900x700")
         self.root.configure(bg="#111111")
         self.root.resizable(False, False)
- self.score = 0
-        self.question_number = 0
-        self.maze_level = 0
- 
-        # ----------------------------------------------------
-        # QUESTIONS
-        # First 5 are easy, then they get harder. Following a tutorial on how to make questions randomized so code is half done and not functional yet
-        # ----------------------------------------------------
-        self.questions = [
-            {
-                "question": "What is 2 + 2?",
-                "answers": ["3", "4", "5", "6"],
-                "correct": "4"
-                
+
+
+# Starts the game
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    game = QuizGame(root)
+    root.mainloop()
